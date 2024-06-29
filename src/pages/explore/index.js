c105952c8175e63784db137014b04c3f40229cd5
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-06-29 22:45:36 +0530

    Hello! Mom

--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -1,77 +1,73 @@
 /** @jsxImportSource @emotion/react */
-import Link from 'next/link'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import { css } from '@emotion/react'
-import { useRouter } from 'next/router'
-import { htmlToText } from 'html-to-text'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { collection, query, where, getDocs } from "firebase/firestore";
+import Link from 'next/link';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { htmlToText } from 'html-to-text';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { collection, query, where, orderBy, limit } from 'firebase/firestore';
 
-import { createPostForUser, filterExplorePosts } from '../../lib/db'
-import { firestore, auth } from '../../lib/firebase'
+import { createPostForUser, filterExplorePosts } from '../../lib/db';
+import { firestore, auth } from '../../lib/firebase';
 
-import Button from '../../components/button'
-import Header from '../../components/header'
-import Spinner from '../../components/spinner'
-import Container from '../../components/container'
-import Search from '../../components/search'
-import ProfileSettingsModal from '../../components/profile-settings-modal'
-import { truncate } from '../../lib/utils'
-import { getPostByID } from '../../lib/db'
+import Button from '../../components/button';
+import Header from '../../components/header';
+import Spinner from '../../components/spinner';
+import Container from '../../components/container';
+import Search from '../../components/search';
+import ProfileSettingsModal from '../../components/profile-settings-modal';
+import { truncate } from '../../lib/utils';
+import { getPostByID } from '../../lib/db';
 
 export default function Explore() {
-  const router = useRouter()
+  const router = useRouter();
 
   const [user, userLoading, userError] = useAuthState(auth);
   const [initPosts, initPostsLoading, initPostsError] = useCollectionData(
-    firestore.collection('posts')
-    .where('published', '==', true)
-    .where('title', '!=', '')
-    .orderBy('title')
-    .limit(15),{ idField: 'id' },
-  )
+    query(
+      collection(firestore, 'posts'),
+      where('published', '==', true),
+      where('title', '!=', ''),
+      orderBy('title'),
+      limit(15)
+    ),
+    { idField: 'id' }
+  );
   const [explorePosts, setExplorePosts] = useState([]);
 
   useEffect(() => {
-    console.log(user, userLoading, userError)
     if (!user && !userLoading && !userError) {
-      router.push('/')
-      return
+      router.push('/');
     }
   }, [router, user, userLoading, userError]);
 
-  // Set initial filteredPosts
   useEffect(() => {
-    (async () => {
+    const fetchPosts = async () => {
+      if (initPosts) {
         let posts = await setPostAuthorProfilePics(initPosts);
         setExplorePosts(posts);
-    })()
-  }, initPosts)
+      }
+    };
+    fetchPosts();
+  }, [initPosts]);
 
-  // Set the profile pics for each author
-  const setPostAuthorProfilePics = async(filteredExplorePosts) => {
-    const postPromises = filteredExplorePosts?.map(async p => {
-      const post = await getPostByID(p.id)
-      const author = await firestore
-        .collection('users')
-        .doc(post.author)
-        .get()
-      post.author = author.data()
+  const setPostAuthorProfilePics = async (filteredExplorePosts) => {
+    const postPromises = filteredExplorePosts?.map(async (p) => {
+      const post = await getPostByID(p.id);
+      const authorDoc = await firestore.collection('users').doc(post.author).get();
+      post.author = authorDoc.data();
       return post;
-    })
-    const posts = postPromises ? await Promise.all(postPromises) : null
-    return posts
-  }
+    });
+    return postPromises ? await Promise.all(postPromises) : [];
+  };
 
-  // Get the searchInput from Search component and do the global search on db
   const getFilteredExplorePosts = async (searchInput) => {
     let filteredExplorePosts = await filterExplorePosts(searchInput);
     filteredExplorePosts = await setPostAuthorProfilePics(filteredExplorePosts);
-    setExplorePosts(filteredExplorePosts)
-    return filteredExplorePosts;
-  }
+    setExplorePosts(filteredExplorePosts);
+  };
 
   return (
     <>
@@ -89,81 +85,81 @@ export default function Explore() {
       {userError ? (
         <>
           <p>Oop, we&apos;ve had an error:</p>
-          <pre>{JSON.stringify(error)}</pre>
+          <pre>{JSON.stringify(userError)}</pre>
         </>
       ) : user ? (
         explorePosts && explorePosts.length > 0 ? (
           <>
-          <Search
-            posts={explorePosts}
-            isGlobalSearch={true}
-            getSearchInput={getFilteredExplorePosts}
-            css={css`
-              margin-left: 0em
-            `}
-          ></Search>
-          <ul
-          css={css`
-            list-style: none;
+            <Search
+              posts={explorePosts}
+              isGlobalSearch={true}
+              getSearchInput={getFilteredExplorePosts}
+              css={css`
+                margin-left: 0em;
+              `}
+            />
+            <ul
+              css={css`
+                list-style: none;
 
-            li {
-              max-width: 25rem;
-              margin: 2.5rem 0;
-            }
-          `}
-        >
-          {explorePosts.map(post => (
-            <li key={post.id}>
-              <Link href={`/${post.author.name}/${post.slug}`}>
-                <a style={{ textDecoration: 'none', color: 'inherit' }}>
-                  <h3
-                    css={css`
-                      font-size: 1rem;
-                      font-weight: 400;
-                      margin-bottom: 0.6rem;
-                    `}
-                  >
-                    {post.title ? htmlToText(post.title) : 'Untitled'}
-                  </h3>
+                li {
+                  max-width: 25rem;
+                  margin: 2.5rem 0;
+                }
+              `}
+            >
+              {explorePosts.map((post) => (
+                <li key={post.id}>
+                  <Link href={`/${post.author.name}/${post.slug}`}>
+                    <a style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <h3
+                        css={css`
+                          font-size: 1rem;
+                          font-weight: 400;
+                          margin-bottom: 0.6rem;
+                        `}
+                      >
+                        {post.title ? htmlToText(post.title) : 'Untitled'}
+                      </h3>
 
-                  <div
-                    css={css`
-                      display: flex;
-                      align-items: center;
-                      color: var(--grey-3);
-                      font-size: 0.9rem;
-                    `}
-                  >
-                    <img
-                      src={post.author.photo}
-                      alt="Profile picture"
-                      css={css`
-                        width: 1.5rem;
-                        border-radius: 1rem;
-                        margin-right: 0.75rem;
-                      `}
-                    />
-                    <p>{post.author.displayName}</p>
-                  </div>
+                      <div
+                        css={css`
+                          display: flex;
+                          align-items: center;
+                          color: var(--grey-3);
+                          font-size: 0.9rem;
+                        `}
+                      >
+                        <img
+                          src={post.author.photo}
+                          alt="Profile picture"
+                          css={css`
+                            width: 1.5rem;
+                            border-radius: 1rem;
+                            margin-right: 0.75rem;
+                          `}
+                        />
+                        <p>{post.author.displayName}</p>
+                      </div>
 
-                  <p
-                    css={css`
-                      color: var(--grey-4);
-                      font-family: 'Newsreader', serif;
-                      line-height: 1.5em;
-                      margin-top: 0.5rem;
-                    `}
-                  >
-                    {post.excerpt
-                      ? htmlToText(post.excerpt)
-                      : truncate(htmlToText(post.content), 25)}
-                  </p>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        </>
+                      <p
+                        css={css`
+                          color: var(--grey-4);
+                          font-family: 'Newsreader', serif;
+                          line-height: 1.5em;
+                          margin-top: 0.5rem;
+                        `}
+                      >
+                        {post.excerpt
+                          ? htmlToText(post.excerpt)
+                          : truncate(htmlToText(post.content), 25)}
+                      </p>
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
         ) : (
           <p>Your really early! Nothing&apos;s posted but you could be the first!</p>
         )
@@ -171,8 +167,9 @@ export default function Explore() {
         <Spinner />
       )}
     </>
-  )
+  );
 }
+
 Explore.getLayout = function Explore(page) {
   return (
     <Container
@@ -186,5 +183,5 @@ Explore.getLayout = function Explore(page) {
       </Head>
       {page}
     </Container>
-  )
-}
+  );
+};
