669cb241964c1d4b669918ecd9e1972edb6f9dfe
Solomon Shalom Lijo <solomon@bublr.life>
2024-07-08 23:02:01 +0530

    Revert "Hello! Mom"
    This reverts commit f1da9ed14f9705a0233a89c9e1693afb0bd0ba8b.

--- src/components/profile-settings-modal.js
+++ src/components/profile-settings-modal.js
@@ -124,8 +124,8 @@ function Editor({ user }) {
               outline: none;
               border-radius: 0.5rem;
               color: inherit;
-              background-color: var(--grey-1);
-            `}
+              background-color: var(--grey-1)
+              `}
             id="profile-country"
             value={clientUser.country}
             onChange={(e) =>
@@ -189,7 +189,6 @@ function Editor({ user }) {
           user.name === clientUser.name &&
           user.displayName === clientUser.displayName &&
           user.about === clientUser.about &&
-          user.country === clientUser.country &&
           !usernameErr
         }
         onClick={async () => {
@@ -223,6 +222,7 @@ function Editor({ user }) {
     </>
   );
 }
+
 
 function ProfileEditor({ uid }) {
   const [user, userLoading, userError] = useDocumentData(
