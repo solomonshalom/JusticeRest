a68e93a5ea44e76d2d176e88ab05792f5e6e3460
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-07-08 18:57:47 +0530

    Hello! Mom

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,104 +1,97 @@
-import firebase, { firestore } from './firebase';
+import firebase, { firestore } from './firebase'
 
 export async function userWithIDExists(id) {
-  const doc = await firestore.collection('users').doc(id).get();
-  return doc.exists;
-}
-
-export async function setUser(id, data) {
-  await firestore.collection('users').doc(id).set({
-    country: data.country, // Ensure to include the country field here
-  });
+  const doc = await firestore.collection('users').doc(id).get()
+  return doc.exists
 }
 
 export async function userWithNameExists(name) {
   const query = await firestore
     .collection('users')
     .where('name', '==', name)
-    .get();
+    .get()
 
-  return !query.empty;
+  return !query.empty
 }
 
 export async function getUserByID(id) {
-  const doc = await firestore.collection('users').doc(id).get();
+  const doc = await firestore.collection('users').doc(id).get()
   if (!doc.exists) {
-    throw { code: 'user/not-found' };
+    throw { code: 'user/not-found' }
   }
 
-  const user = doc.data();
-  const postDocPromises = user.posts.map(postId => getPostByID(postId));
-  user.posts = await Promise.all(postDocPromises);
+  const user = doc.data()
+  const postDocPromises = user.posts.map(postId => getPostByID(postId))
+  user.posts = await Promise.all(postDocPromises)
 
-  return { id: doc.id, ...user };
+  return { id: doc.id, ...user }
 }
 
 export async function getUserByName(name) {
-  try {
-    const query = await firestore
-      .collection('users')
-      .where('name', '==', name)
-      .get();
+  const query = await firestore
+    .collection('users')
+    .where('name', '==', name)
+    .get()
 
-    if (query.empty || !query.docs[0].exists) {
-      throw { code: 'user/not-found' };
-    }
-
-    const user = { id: query.docs[0].id, ...query.docs[0].data() };
-    const postDocPromises = user.posts.map(postId => getPostByID(postId));
-    user.posts = await Promise.all(postDocPromises);
-
-    return user;
-  } catch (error) {
-    console.error('Error fetching user by name:', error);
-    throw error; // Re-throw the error to propagate it up the call stack
+  if (query.empty || !query.docs[0].exists) {
+    throw { code: 'user/not-found' }
   }
+
+  const user = { id: query.docs[0].id, ...query.docs[0].data() }
+  const postDocPromises = user.posts.map(postId => getPostByID(postId))
+  user.posts = await Promise.all(postDocPromises)
+
+  return user
 }
 
 export async function getPostByID(id) {
-  const doc = await firestore.collection('posts').doc(id).get();
+  const doc = await firestore.collection('posts').doc(id).get()
   if (!doc.exists) {
-    throw { code: 'post/not-found' };
+    throw { code: 'post/not-found' }
   }
 
-  return { id: doc.id, ...doc.data() };
+  return { id: doc.id, ...doc.data() }
 }
 
 export async function removePostForUser(uid, pid) {
-  await firestore.collection('posts').doc(pid).delete();
+  await firestore.collection('posts').doc(pid).delete()
   firestore
     .collection('users')
     .doc(uid)
-    .update({ posts: firebase.firestore.FieldValue.arrayRemove(pid) });
+    .update({ posts: firebase.firestore.FieldValue.arrayRemove(pid) })
 }
 
 export async function postWithIDExists(id) {
-  const doc = await firestore.collection('posts').doc(id).get();
-  return doc.exists;
+  const doc = await firestore.collection('posts').doc(id).get()
+  return doc.exists
 }
 
 export async function postWithUsernameAndSlugExists(username, slug) {
-  const user = await getUserByName(username);
-  return user.posts.find(post => post.slug === slug);
+  const user = await getUserByName(username)
+  return user.posts.find(post => post.slug === slug)
 }
 
 export async function postWithUserIDAndSlugExists(uid, slug) {
-  const user = await getUserByID(uid);
-  return user.posts.find(post => post.slug === slug);
+  const user = await getUserByID(uid)
+  return user.posts.find(post => post.slug === slug)
 }
 
 export async function getPostByUsernameAndSlug(username, slug) {
-  const user = await getUserByName(username);
-  const post = user.posts.find(post => post.slug === slug);
+  const user = await getUserByName(username)
+  const post = user.posts.find(post => post.slug === slug)
   if (!post) {
-    throw { code: 'post/not-found' };
+    throw { code: 'post/not-found' }
   }
 
-  return post;
+  return post
+}
+
+export async function setUser(id, data) {
+  await firestore.collection('users').doc(id).set(data)
 }
 
 export async function setPost(id, data) {
-  await firestore.collection('posts').doc(id).set(data);
+  await firestore.collection('posts').doc(id).set(data)
 }
 
 export async function createPostForUser(userId) {
@@ -109,33 +102,14 @@ export async function createPostForUser(userId) {
     author: userId,
     published: false,
     lastEdited: firebase.firestore.Timestamp.now(),
-  });
+  })
 
-  await firestore.collection('posts').doc(doc.id).update({ slug: doc.id });
+  await firestore.collection('posts').doc(doc.id).update({ slug: doc.id })
 
   await firestore
     .collection('users')
     .doc(userId)
-    .update({ posts: firebase.firestore.FieldValue.arrayUnion(doc.id) });
+    .update({ posts: firebase.firestore.FieldValue.arrayUnion(doc.id) })
 
-  return doc.id;
-}
-
-export async function filterExplorePosts(searchInput) {
-  let posts = [];
-  const snapshot = await firebase
-    .firestore()
-    .collection('posts')
-    .where('published', '==', true)
-    .where('title', '!=', '')
-    .where('title', '>=', searchInput)
-    .where('title', '<', searchInput + 'z')
-    .get();
-  snapshot.docs.map(doc => {
-    posts.push({
-      id: doc.id,
-      ...doc.data(),
-    });
-  });
-  return posts;
+  return doc.id
 }
