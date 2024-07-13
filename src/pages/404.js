d31e4d2e778ceff36171fbb16f30a61fb48058d9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-13 11:55:19 +0000

    edit_file
    # Path: [1. Process .js files]/for_each/edit_file
    # UUID: 0cc84714-22fc-4a08-9a3c-6c7ebe7429aa
    
    # Changed files:
    src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,17 +4,21 @@ import { css } from '@emotion/react'
 
 import Container from '../components/container'
 
-export default function NotFound() {
+interface NotFoundProps {
+  maxWidth?: string;
+}
+
+const NotFound: React.FC<NotFoundProps> = ({ maxWidth = "640px" }) => {
   return (
-    <Container maxWidth="640px">
+    <Container maxWidth={maxWidth}>
       <Head>
         <title>Not Found</title>
 
         <script defer src="https://cloud.umami.is/script.js" data-website-id="a0cdb368-20ae-4630-8949-ac57917e2ae3"></script>
-      
+
         <link rel="manifest" href="https://www.justice.rest/justicerest.webmanifest" />
         <meta name="mobile-web-app-capable" content="yes" />
-      
+
       </Head>
       <h1>404</h1>
       <p
@@ -27,3 +31,5 @@ export default function NotFound() {
     </Container>
   )
 }
+
+export default NotFound;
