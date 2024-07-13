65f7d4c034503692fc50b8f398b1debead1bb6e1
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-13 11:30:58 +0000

    edit_file
    # Path: [1. Process .js files]/for_each/edit_file
    # UUID: 3dfbc578-3f63-490c-964b-ad726df2d880
    
    # Changed files:
    src/lib/firebase-config.js

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -1,6 +1,24 @@
 // ENV configs for firebase (Super Important!)
 
-export const FIREBASE_CONFIG = {
+export enum FirebaseConfigKeys {
+  API_KEY = "apiKey",
+  AUTH_DOMAIN = "authDomain",
+  PROJECT_ID = "projectId",
+  STORAGE_BUCKET = "storageBucket",
+  MESSAGING_SENDER_ID = "messagingSenderId",
+  APP_ID = "appId",
+}
+
+export interface FirebaseConfig {
+  [FirebaseConfigKeys.API_KEY]: string;
+  [FirebaseConfigKeys.AUTH_DOMAIN]: string;
+  [FirebaseConfigKeys.PROJECT_ID]: string;
+  [FirebaseConfigKeys.STORAGE_BUCKET]: string;
+  [FirebaseConfigKeys.MESSAGING_SENDER_ID]: string;
+  [FirebaseConfigKeys.APP_ID]: string;
+}
+
+export const FIREBASE_CONFIG: FirebaseConfig = {
   apiKey: "AIzaSyBL4FKAG_mofaxZiG2q0j3543Ims_80GaA",
   authDomain: "jrle-216c1.firebaseapp.com",
   projectId: "jrle-216c1",
@@ -9,4 +27,4 @@ export const FIREBASE_CONFIG = {
   appId: "1:21313569188:web:648724b2e6d6cb8bdd4131",
 }
 
-export default FIREBASE_CONFIG
+export default FIREBASE_CONFIG;
