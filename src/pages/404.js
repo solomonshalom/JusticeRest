e9f653e0deaff22031356477d09c0ddb1098c985
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-13 11:29:38 +0000

    edit_file
    # Path: [1. Process .js files]/for_each/edit_file
    # UUID: 17115688-937c-44af-8b73-a7d4a027fffe
    
    # Changed files:
    src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import Head from 'next/head'
 import { css } from '@emotion/react'
-
 import Container from '../components/container'
 
-export default function NotFound() {
+interface ContainerProps {
+  maxWidth: string;
+}
+
+export default function NotFound(): JSX.Element {
   return (
     <Container maxWidth="640px">
       <Head>
         <title>Not Found</title>
 
         <script defer src="https://cloud.umami.is/script.js" data-website-id="a0cdb368-20ae-4630-8949-ac57917e2ae3"></script>
-      
+
         <link rel="manifest" href="https://www.justice.rest/justicerest.webmanifest" />
         <meta name="mobile-web-app-capable" content="yes" />
 
         <script>
-              if (typeof navigator.serviceWorker !== 'undefined') {
-                navigator.serviceWorker.register('https://www.justice.rest/sw.js')
-              }
-       </script>
+          {`if (typeof navigator.serviceWorker !== 'undefined') {
+            navigator.serviceWorker.register('https://www.justice.rest/sw.js')
+          }`}
+        </script>
 
       </Head>
       <h1>404</h1>
