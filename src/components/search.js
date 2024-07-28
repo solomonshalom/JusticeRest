15162c42134b3862ee5f3dc2d4d05b7bb9187e04
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-07-28 06:21:13 -0700

    Hello! Mom

--- src/components/search.js
+++ src/components/search.js
@@ -65,9 +65,19 @@ export default function Search(props) {
     <div
     css={exploreSearchBarStyles}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff"css={css`
+      <svg xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff" 
+      css={css`
           position: absolute;
           margin: 0.8em
+
+                path {
+                  stroke: black;
+                }
+
+                @media (prefers-color-scheme: dark) {
+                  path {
+                    stroke: white;
+                  }
         `}><path stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="m17 17 4 4M3 11a8 8 0 1 0 16 0 8 8 0 0 0-16 0Z"></path>
       </svg>
       <input
