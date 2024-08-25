4d884709a10f6601a464b1a44a477da9b541505d
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-08-25 23:48:48 +0530

    Jesus W!

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/',
+        destination: '/index.html',
+        permanent: true, // Set to true if the redirect is permanent
+      },
+      {
         source: '/@:username',
         destination: '/:username',
         permanent: true,
